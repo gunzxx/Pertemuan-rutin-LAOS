8b3000c327b8c82bc9a45eda7d203041c435bfd2
gunzxx <ggm5850@gmail.com>
2023-06-06 22:14:16 +0700

    update index

--- index.php
+++ index.php
@@ -27,45 +27,52 @@
 
     <section class="products">
         <div class="card-container">
-            <div class="card-image">
-                <img src="./img/produk0.png" alt="">
+            <div class="card-body">
+                <div class="card-image">
+                    <img src="./img/produk0.png" alt="">
+                </div>
+                <div class="card-content">
+                    <h1>Produk 1</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+                </div>
             </div>
-            <div class="card-content">
-                <h1>Produk 1</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+            <div class="card-footer">
+                <a class="btn btn-danger" href="./delete.php">Hapus</a>
+                <a class="btn btn-second" href="./edit.php">Edit</a>
                 <a class="btn btn-main" href="./buy.php">Beli</a>
             </div>
         </div>
         
         <div class="card-container">
-            <div class="card-image">
-                <img src="./img/produk0.png" alt="">
+            <div class="card-body">
+                <div class="card-image">
+                    <img src="./img/produk0.png" alt="">
+                </div>
+                <div class="card-content">
+                    <h1>Produk 1</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minus debitis tempore culpa.</p>
+                </div>
             </div>
-            <div class="card-content">
-                <h1>Produk 1</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+            <div class="card-footer">
+                <a class="btn btn-danger" href="./delete.php">Hapus</a>
+                <a class="btn btn-second" href="./edit.php">Edit</a>
                 <a class="btn btn-main" href="./buy.php">Beli</a>
             </div>
         </div>
-
+        
         <div class="card-container">
-            <div class="card-image">
-                <img src="./img/produk0.png" alt="">
+            <div class="card-body">
+                <div class="card-image">
+                    <img src="./img/produk0.png" alt="">
+                </div>
+                <div class="card-content">
+                    <h1>Produk 1</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+                </div>
             </div>
-            <div class="card-content">
-                <h1>Produk 1</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
-                <a class="btn btn-main" href="./buy.php">Beli</a>
-            </div>
-        </div>
-
-        <div class="card-container">
-            <div class="card-image">
-                <img src="./img/produk0.png" alt="">
-            </div>
-            <div class="card-content">
-                <h1>Produk 1</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+            <div class="card-footer">
+                <a class="btn btn-danger" href="./delete.php">Hapus</a>
+                <a class="btn btn-second" href="./edit.php">Edit</a>
                 <a class="btn btn-main" href="./buy.php">Beli</a>
             </div>
         </div>
