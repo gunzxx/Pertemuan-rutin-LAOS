4998da7b3321ff79f03237712fa65fe645bbd498
gunzxx <ggm5850@gmail.com>
2023-06-06 22:04:04 +0700

    create index page

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAOS Shop</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <nav id="nav">
+        <div class="nav-col">
+            <img src="./img/logo.svg" alt="">
+            <h1>LAOS Shop</h1>
+        </div>
+        <div class="nav-col col2">
+            <a href="./index.php">Home</a>
+            <a class="btn btn-main" href="./create.php">Add</a>
+        </div>
+    </nav>
+
+    <section class="jumbotron">
+        <img src="./img/banner.jpg" alt="banner">
+    </section>
+
+    <section class="products">
+        <div class="card-container">
+            <div class="card-image">
+                <img src="./img/produk0.png" alt="">
+            </div>
+            <div class="card-content">
+                <h1>Produk 1</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+                <a class="btn btn-main" href="./buy.php">Beli</a>
+            </div>
+        </div>
+        
+        <div class="card-container">
+            <div class="card-image">
+                <img src="./img/produk0.png" alt="">
+            </div>
+            <div class="card-content">
+                <h1>Produk 1</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+                <a class="btn btn-main" href="./buy.php">Beli</a>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card-image">
+                <img src="./img/produk0.png" alt="">
+            </div>
+            <div class="card-content">
+                <h1>Produk 1</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+                <a class="btn btn-main" href="./buy.php">Beli</a>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card-image">
+                <img src="./img/produk0.png" alt="">
+            </div>
+            <div class="card-content">
+                <h1>Produk 1</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate minima nam iusto iure nihil, iste vero quis animi nulla hic minus debitis tempore culpa.</p>
+                <a class="btn btn-main" href="./buy.php">Beli</a>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
